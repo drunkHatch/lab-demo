c52c1f8cfa4d15f3bc49db588ac9527d11856384
Yuqi Zhang <yuqi8@ualberta.ca>
2019-01-10 14:25:50 -0700

    lab 111

--- lab1.py
+++ lab1.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
 import requests as req
 
 print(req.__version__)
+
+r = req.get("http://google.com/")
+#print(r.text)
+
+my_py = req.get("https://github.com/drunkHatch/lab-demo.git")
+print(my_py.text)
